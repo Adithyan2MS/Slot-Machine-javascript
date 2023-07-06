7a2c8ec1f1c4dd3d516a48a536099ee2546a6578
Adithyan2MS <adithyan2ms@gmail.com>
2023-07-06 14:21:14 +0530

    Removed node_modules

---
+++ project.js
@@ -0,0 +1,100 @@
+const prompt = require("prompt-sync")()
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+
+const SYMBOLS_VALUES = {
+    "A":5,
+    "B":4,
+    "C":3,
+    "D":2
+}
+
+
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ")
+        const numberDepositAmount = parseFloat(depositAmount)
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, Try Again!");
+        }else{
+            return numberDepositAmount
+        }
+    }
+}
+const getNumberOfLines = () => {
+    while (true) {
+        const Lines = prompt("Enter the Number of Lines to bet on (1-3): ")
+        const numberOfLines = parseFloat(Lines)
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines >3){
+            console.log("Invalid  Line number, Try Again!");
+        }else{
+            return numberOfLines
+        }
+    }
+}
+
+const getBet = (balance,lines) => {
+    while (true) {
+        const Bet = prompt("Enter the Bet per line: ")
+        const numberBet = parseFloat(Bet)
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines){
+            console.log("Invalid  Bet, Try Again!");
+        }else{
+            return numberBet
+        }
+    }
+}
+
+const spin = () =>{
+    const symbols = [];
+    for(const [symbol,count] of Object.entries(SYMBOLS_COUNT)){
+        for (let i = 0; i < count; i++){
+            symbols.push(symbol)
+        }
+    }
+    
+    const reels = [];
+    for (let i = 0; i < COLS; i++){
+        reels.push([])
+        const reelSymbols = [...symbols]
+        for (let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex]
+            reels[i].push(selectedSymbol)
+            reelSymbols.splice(randomIndex,1)
+        }
+    }
+    return reels
+}
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for(let i = 0; i < ROWS; i++){
+        rows.push([])
+        for(let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+    return rows
+}
+
+let balance = deposit()
+const numberOfLines = getNumberOfLines()
+const Bet = getBet(balance,numberOfLines)
+const reels = spin()
+const rows = transpose(reels)
+console.log(reels);
+console.log(rows);
